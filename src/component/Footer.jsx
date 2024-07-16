0aa5b6f4fed9c49ca698272882f107476462fb78
muzamil1233 <muzamiln213@gmail.com>
2024-07-16 18:39:40 +0530

    initial commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,38 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = 'https://avatars.githubusercontent.com/u/147420243?s=400&u=7c0d1d9a7a67ddc432485904574fe73fe27f2470&v=4';
+
+const Footer = () => {
+  return (
+    <Box
+    
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very cheap price.
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Our Founder</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
